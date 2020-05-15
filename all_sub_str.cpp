f3b55adda663690f14e852824a737ddda9559919
99shailesh <99shaileshu@gmail.com>
2020-05-15 00:05:34 -0400

    day 2 only

--- all_sub_str.cpp
+++ all_sub_str.cpp
@@ -16,9 +16,12 @@ int main(int argc, char const *argv[])
 		int count=0,m;
 		cin>>m;
 		cin>>str;
+		/*
+		 o(n^2)
+		*/
 		for(int i=0;i<m;i++)
 		{
-			for(int j=1;j<=m;j++)
+			for(int j=1;j<=m-i;j++)
 			{
 
 				str1=str.substr(i,j);
@@ -32,6 +35,20 @@ int main(int argc, char const *argv[])
 			}
 		}
 		cout<<count<<endl;
+		/*
+		 o(n)
+		 c is count of 1 present in string
+		 total subbstring sarting and ending with 1 will be c*(c+1)/2
+		*/
+		long long int c=0;
+		for(int i=0;i<m;i++)
+		{
+			if(str[i]=='1')
+			{
+				c=c+1;
+			}
+		}
+		cout<<c*(c+1)/2<<endl;
 	}
 	return 0;
 }
